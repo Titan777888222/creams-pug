13bc85d82632d6061b57bdc8ce5dc9a2139ccf5a
Titan777888222 <rbrui777@mail.ru>
2022-03-20 09:26:48 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+'use strict';
+$(document).ready(function(){
+    $('.header-burger').click(function(event){
+        $('.header__burger_menu, .header-burger').toggleClass('active');
+    });
+});
---
+++ public/js/application.js
@@ -0,0 +1,7 @@
+
+'use strict';
+$(document).ready(function(){
+    $('.header-burger').click(function(event){
+        $('.header__burger_menu, .header-burger').toggleClass('active');
+    });
+});
